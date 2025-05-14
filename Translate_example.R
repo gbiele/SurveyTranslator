beaa44765054fc03eebaaac0307287958f1a417f
Guido Philipp Emmanuel Biele <Guido.Biele@fhi.no>
2025-05-14 15:48:08 +0200

    organize files

--- Translate_example.R
+++ Translate_example.R
@@ -1,2 +1,2 @@
 library(here)
-translated_items = llm_translate(source_file = here("data/BCFPI_items.txt"), batch_size = 2)
+translated_items = llm_translate(source_file = here("xdata/BCFPI_items.txt"), batch_size = 15)
